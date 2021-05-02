9afb258c351166c14d56463e6fb7e415eeb8253c
LordCam2005 <81117319+LordCam2005@users.noreply.github.com>
2021-05-03 10:55:12 +1200

    sprite

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -1,52 +1,21 @@
-import random
 import arcade
 
-# constants
-sprite_scaling_player = 0.5
-sprite_scaling_coin = 0.2
-coin_count = 50
+WIDTH = 800
+HEIGHT = 600
+TITLE = "sprite"
 
-screen_height = 600
-screen_width = 800
-
-
-
-class MyGame(arcade.Window):
+class Game(arcade.Window):
     def __init__(self):
-        super().__init__(screen_width, screen_height, "sprite shenanigans")
-
-        self.player_list = None
-        self.coin_list = None
-
+        super().__init__(WIDTH, HEIGHT, TITLE)
         self.player_sprite = None
-        self.score = 0
-
-        self.set_mouse_visible(False)
-
-        arcade.set_background_color(arcade.color.RUBY_RED)
 
     def setup(self):
-        self.player_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
-
-        self.score = 0
-
-        self.player_sprite = arcade.Sprite("charecter.png", sprite_scaling_player)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
+        self.player_sprite = arcade.Sprite("./player_action1.png", center_x = WIDTH/2, center_y = HEIGHT/2)
 
     def on_draw(self):
         arcade.start_render()
+        self.player_sprite.draw()
 
-        self.coin_list.draw()
-        self.player_list.draw()
-
-def main():
-    window = MyGame()
-    window.setup
-    arcade.run()
-
-if __name__ == "__main__":
-    main()
-    
+window = Game()
+window.setup() 
+arcade.run()
